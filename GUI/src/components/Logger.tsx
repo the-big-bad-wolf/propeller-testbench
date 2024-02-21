438dd285def8834f38617368e4b8ee5ef3d2560e
The Big Bad Wolf <78231350+the-big-bad-wolf@users.noreply.github.com>
2024-02-21 15:31:19 +0100

    update chart

--- GUI/src/components/Logger.tsx
+++ GUI/src/components/Logger.tsx
@@ -1,4 +1,6 @@
-import { createSignal, onCleanup } from "solid-js";
+import { createSignal, onCleanup, onMount } from "solid-js";
+import { Chart, Title, Tooltip, Legend, Colors } from "chart.js";
+import { Line } from "solid-chartjs";
 
 interface WebSocketData {
 	i: number;
@@ -6,10 +8,27 @@ interface WebSocketData {
 	// Add more properties as needed
 }
 
+interface chartData {
+	labels: string[];
+	datasets: {
+		label: string;
+		data: number[];
+	}[];
+}
+
 const WebSocketComponent = () => {
 	const [force_measurements, setData] = createSignal<number[] | null>(null);
-	const socket = new WebSocket("ws://192.168.137.157:81");
+	const [chartData, setChartData] = createSignal<chartData>({
+		labels: ["January", "February", "March", "April", "May"],
+		datasets: [
+			{
+				label: "Force",
+				data: [50, 60, 70, 80, 90],
+			},
+		],
+	});
 
+	const socket = new WebSocket("ws://192.168.137.157:81");
 	socket.onerror = event => {
 		console.error("WebSocket error observed:", event);
 		socket.close();
@@ -23,9 +42,17 @@ const WebSocketComponent = () => {
 		// Do something with the parsed message
 		console.log(data);
 		setData(data.force_measurements);
+
+		const newChartData = {
+			labels: [...chartData().labels, data.i.toString()],
+			datasets: [{ label: "Force", data: [...chartData().datasets[0].data, data.i] }],
+		};
+		setChartData(newChartData);
 	};
 
-	// Clean up the WebSocket connection when the component is unmounted
+	onMount(() => {
+		Chart.register(Title, Tooltip, Legend, Colors);
+	});
 	onCleanup(() => {
 		socket.close();
 	});
@@ -34,6 +61,16 @@ const WebSocketComponent = () => {
 		<div>
 			<h1>Live Data Stream</h1>
 			<p>{force_measurements()}</p>
+			<Line
+				type="line"
+				data={chartData()}
+				options={{
+					responsive: false,
+					maintainAspectRatio: false,
+				}}
+				width={720}
+				height={360}
+			/>
 		</div>
 	);
 };
